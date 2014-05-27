ae9c1587950fbcd58959e9e9ffafdfeec50f7bb0
wayne5540 <w5540@yahoo.com.tw>
2014-05-27 16:54:27 +0800

    admin inde

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-
+  def login_required
+    if current_user.blank?
+      respond_to do |format|
+        format.html  {
+          authenticate_user!
+        }
+        format.js{
+          render :partial => "common/not_logined"
+        }
+        format.all {
+          head(:unauthorized)
+        }
+      end
+    end  
+  end
 
   protected
   def configure_permitted_parameters
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
 module ProductsHelper
+
+  def render_product_name_path(product)
+    product.name
+  end
+  def render_product_desc(product)
+    product.description
+  end
+  
 end
+
