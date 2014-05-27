d90261ae76a092d6dd4aeca704bc21cf70248b21
wayne5540 <w5540@yahoo.com.tw>
2014-05-27 16:29:34 +0800

    add role, associate role and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+
+
+  has_many :user_roles, :dependent => :destroy
+  has_many :roles, :through => :user_roles
+
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140527073035) do
+ActiveRecord::Schema.define(version: 20140527080238) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_roles", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
