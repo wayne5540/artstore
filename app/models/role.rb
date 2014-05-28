4c36f5182c4523d4e0bf53f3de206570b7d6aac4
wayne5540 <w5540@yahoo.com.tw>
2014-05-28 10:04:34 +0800

    add role system

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,9 @@
 class Role < ActiveRecord::Base
   has_many :user_roles, :dependent => :destroy
-  has_many :users, :through => :user_roles  
+  has_many :users, :through => :user_roles
+
+
+  def self.has_role?(role)
+    where(:name => role).present?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
 
-
   has_many :user_roles, :dependent => :destroy
   has_many :roles, :through => :user_roles
 
@@ -10,4 +9,23 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :set_default_role
+
+  def admin?
+    is?(:admin)
+  end
+
+
+  def is?(role)
+    roles.has_role?(role)
+  end
+
+
+
+  def set_default_role
+    role = Role.find_by(name: "user")
+    self.roles << role
+  end
+
 end
