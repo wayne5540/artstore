6b8bb1f672f01efb8cb3d9a2dd5871f78010cc20
wayne5540 <w5540@yahoo.com.tw>
2014-05-28 11:05:43 +0800

    add category id validate to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
   belongs_to :category
   validates :name, presence: true
+  validates :category_id, presence: true
 end
