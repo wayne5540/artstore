2aa25d6fa5328cfe06d2a0de5230e6c7c26d7e95
wayne5540 <w5540@yahoo.com.tw>
2014-05-28 14:08:48 +0800

    add spec model, associate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ActiveRecord::Base
-  mount_uploader :image, ProductImageUploader
+  has_many :specs, :dependent => :destroy
   belongs_to :category
   validates :name, presence: true
   validates :category_id, presence: true
+
+  # carriewave
+  mount_uploader :image, ProductImageUploader
+
+
 end
