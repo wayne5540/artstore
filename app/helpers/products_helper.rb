5204d3be07deb19518821d9a73535c72bec72436
wayne5540 <w5540@yahoo.com.tw>
2014-05-28 13:53:30 +0800

    xdite responded

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,16 +1,14 @@
 module ProductsHelper
 
-  def render_product_name_path(product)
+  def render_product_name(product)
     link_to(product.name, product_path(product))
   end
   def render_product_desc(product)
     product.description
   end
+
   def render_product_image(product)
-    product.image.present? ? product.image.url : 'no-image.jpeg'
-  end
-  def render_product_image_path(product)
-    image_tag(render_product_image(product), :alt => product.name)
+    image_tag((product.image.url||'no-image.jpeg'), :alt => product.name)
   end
 
 
