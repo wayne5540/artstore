353a74f44fd914800761d1598636e6c4773fd304
wayne5540 <w5540@yahoo.com.tw>
2014-06-05 13:41:16 +0800

    :gem: annotate

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: carts
+#
+#  id               :integer          not null, primary key
+#  created_at       :datetime
+#  updated_at       :datetime
+#  cart_items_count :integer          default(0)
+#
+
 class Cart < ActiveRecord::Base
   has_many :items, :class_name => "CartItem", :dependent => :destroy
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :integer          not null, primary key
+#  cart_id    :integer
+#  product_id :integer
+#  spec_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class CartItem < ActiveRecord::Base
   belongs_to :cart, :counter_cache => true
   belongs_to :product
