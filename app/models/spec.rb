92ac2c19b0d8ab68f55de94e1af5fde620fc80fe
wayne5540 <w5540@yahoo.com.tw>
2014-06-05 13:39:20 +0800

    annotate

--- app/models/spec.rb
+++ app/models/spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: specs
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  detail     :text
+#  inventory  :integer          default(0)
+#  product_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Spec < ActiveRecord::Base
   belongs_to :product
   validates :name, presence: true
