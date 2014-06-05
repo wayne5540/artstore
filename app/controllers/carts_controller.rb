983b772e5db1ebde21e4dd2103246da785a94a1d
wayne5540 <w5540@yahoo.com.tw>
2014-06-05 12:24:13 +0800

    carts done

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,6 +3,7 @@ class CartsController < ApplicationController
   def index
     @cart = current_cart
     @items = @cart.items
+    @sum = @items.inject(0){|sum, item| sum + item.product.price }
   end
 
 end
