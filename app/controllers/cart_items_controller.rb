c2914ba486086f5409814815935474c41b5b2880
wayne5540 <w5540@yahoo.com.tw>
2014-06-05 16:50:03 +0800

    cart_item destroy

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -13,6 +13,11 @@ class CartItemsController < ApplicationController
     end
 
   end
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to carts_path
+  end
 
 
 
