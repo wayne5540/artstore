9753f4a371ec8c8f1577fc523763f0fe3897aa89
wayne5540 <w5540@yahoo.com.tw>
2014-06-06 12:20:15 +0800

    order show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,15 @@
 class OrdersController < ApplicationController
 
+  before_action :login_required
+  before_action :find_order, only: [:show]
+  before_action :auth_user, only: [:show]
+
+  def show
+    @items = @order.items.includes(:product, :spec)
+    @detail = @order.order_detail
+  end
+
+
   def new
     @order = current_user.orders.new
     @order.build_order_detail
@@ -12,7 +22,7 @@ class OrdersController < ApplicationController
     if @order.process
       reset_cart
       flash[:success] = "成功新增訂單"
-      redirect_to root_path
+      redirect_to order_path(@new_order)
     else
       render :new
       flash[:warning] = "訂單新增失敗"
@@ -25,6 +35,14 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(order_detail_attributes: [:id, :billing_name, :billing_phone_number, :billing_address,
                                    :shipping_name, :shipping_phone_number, :shipping_address])
+  end
+
+  def find_order
+    @order = Order.find(params[:id])
+  end
+
+  def auth_user
+    @order.user == current_user
   end
 
 end
