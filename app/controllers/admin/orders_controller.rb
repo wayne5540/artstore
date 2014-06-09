2e1b5ce8ff5710a89decd201aa5a5d009b33e741
wayne5540 <w5540@yahoo.com.tw>
2014-06-09 11:04:29 +0800

    Order next_step move to controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -13,8 +13,33 @@ class Admin::OrdersController < ApplicationController
     @returned_orders = @orders.returned
   end
 
+  # def next_status
+  #   @order.next_step
+  #   redirect_to admin_orders_path
+  # end
+
   def next_status
-    @order.next_step
+    case @order.status
+    when "unpaid"
+      @order.set_status("paid")
+      @order.set_order_paid("by_admin")
+      flash[:success] = "成功設定為已付款"
+    when "paid"
+      @order.set_status("shipping")
+      flash[:success] = "成功設定為運送中"
+      OrderMailer.shipping_notification(@order, @order.user.email).deliver
+    when "shipping"
+      @order.set_status("shipped")
+      flash[:success] = "成功設定為已送達"
+    when "shipped"
+      @order.set_status("returned")
+      flash[:success] = "成功設定為退貨訂單"
+    when "returned"
+      #TODO can't change to other status, should show some message
+      flash[:warning] = "Last Step"
+    else
+      flash[:warning] = "請使用正常的付款方式"
+    end
     redirect_to admin_orders_path
   end
 
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -7,4 +7,11 @@ class OrderMailer < ActionMailer::Base
     @link = order_url(@order)
     mail(to: email, subject: "您的Artstore訂單已成立，我們將盡快為您出貨。")
   end
+
+  def shipping_notification(order, email)
+    @order = order
+    @email = email
+    @link = order_url(@order)
+    mail(to: email, subject: "您的Artstore訂單已出貨。")
+  end
 end
